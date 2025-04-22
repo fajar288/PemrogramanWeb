6d71f615126dbb15821a460c8418d384071400b3
fajar288 <fajrzprayoga@gmail.com>
2025-04-22 22:37:06 +0700

    first commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">About us</a>
+        <a href="#">Contact</a>
+    </nav>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/list', function () {
+    return view('list_product');
+});
